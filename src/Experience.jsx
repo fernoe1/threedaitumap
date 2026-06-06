3e72793910567a95e73ff84d86cd5a7b621a2e0b
fernoe1 <krutoytemirlan2007@mail.ru>
2026-06-06 22:38:42 +0500

    feat: new model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,13 +1,17 @@
 import { OrbitControls } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 const Experience = () => {
+    const model = useLoader(GLTFLoader, './aituMap.glb')
+
     return <>
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 } >
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 3.0 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <primitive object={ model.scene } scale={ 0.1 } />
     </>
 };
 
